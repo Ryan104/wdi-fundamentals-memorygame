3c49d65a6bc3bc88cd5089b8d620704314a41f85
Ryan Elliott <18182439+Ryan104@users.noreply.github.com>
2017-08-28 15:32:22 -0600

    Add game logic

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-const cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("Congrats! You found a match!");
-} else {
-	alert("Sorry, try again...");
+const checkForMatch = () => {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Congrats! You found a match!");
+	} else {
+		alert("Sorry, try again...");
+	}
 }
+
+const flipCard = (cardId) => {
+	const card = cards[cardId];
+	cardsInPlay.push(card);
+	console.log('User flipped ' + card);
+};
+
+flipCard(0);
+flipCard(1);
+
+checkForMatch();
