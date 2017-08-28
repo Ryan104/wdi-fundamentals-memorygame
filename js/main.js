b8f97a094aaaa71b78db5601ba0a3cf27d809261
Ryan Elliott <18182439+Ryan104@users.noreply.github.com>
2017-08-28 11:51:55 -0600

    Create game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const cardOne = "queen";
+const cardTwo = "queen";
+const cardThree = "king";
+const cardFour = "king";
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardTwo);
+
