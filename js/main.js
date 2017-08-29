b71328a64c393b8b52e126f0eb490049ced66408
Ryan Elliott <18182439+Ryan104@users.noreply.github.com>
2017-08-29 11:09:19 -0600

    Create game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	}
+];
+
 let cardsInPlay = [];
 
 const checkForMatch = () => {
@@ -12,8 +34,9 @@ const checkForMatch = () => {
 
 const flipCard = (cardId) => {
 	const card = cards[cardId];
-	cardsInPlay.push(card);
-	console.log('User flipped ' + card);
+	cardsInPlay.push(card.rank);
+	console.log('User flipped ' + card.rank);
+	console.log(card.cardImage);
 };
 
 flipCard(0);
