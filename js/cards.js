f837964b361fdf74737c887bea13e90e741cd8f0
Ryan Elliott <18182439+Ryan104@users.noreply.github.com>
2017-09-04 00:48:13 -0600

    Add extra cards

---
+++ js/cards.js
@@ -0,0 +1,122 @@
+const cards = [
+	{
+	rank: "10",
+	suit: "diamonds",
+	cardImage: "images/10_of_diamonds.png"
+	},
+	{
+	rank: "10",
+	suit: "hearts",
+	cardImage: "images/10_of_hearts.png"
+	},
+	{
+	rank: "3",
+	suit: "diamonds",
+	cardImage: "images/3_of_diamonds.png"
+	},
+	{
+	rank: "3",
+	suit: "hearts",
+	cardImage: "images/3_of_hearts.png"
+	},
+	{
+	rank: "4",
+	suit: "diamonds",
+	cardImage: "images/4_of_diamonds.png"
+	},
+	{
+	rank: "4",
+	suit: "hearts",
+	cardImage: "images/4_of_hearts.png"
+	},
+	{
+	rank: "5",
+	suit: "diamonds",
+	cardImage: "images/5_of_diamonds.png"
+	},
+	{
+	rank: "5",
+	suit: "hearts",
+	cardImage: "images/5_of_hearts.png"
+	},
+	{
+	rank: "6",
+	suit: "diamonds",
+	cardImage: "images/6_of_diamonds.png"
+	},
+	{
+	rank: "6",
+	suit: "hearts",
+	cardImage: "images/6_of_hearts.png"
+	},
+	{
+	rank: "7",
+	suit: "diamonds",
+	cardImage: "images/7_of_diamonds.png"
+	},
+	{
+	rank: "7",
+	suit: "hearts",
+	cardImage: "images/7_of_hearts.png"
+	},
+	{
+	rank: "8",
+	suit: "diamonds",
+	cardImage: "images/8_of_diamonds.png"
+	},
+	{
+	rank: "8",
+	suit: "hearts",
+	cardImage: "images/8_of_hearts.png"
+	},
+	{
+	rank: "9",
+	suit: "diamonds",
+	cardImage: "images/9_of_diamonds.png"
+	},
+	{
+	rank: "9",
+	suit: "hearts",
+	cardImage: "images/9_of_hearts.png"
+	},
+	{
+	rank: "ace",
+	suit: "diamonds",
+	cardImage: "images/ace_of_diamonds.png"
+	},
+	{
+	rank: "ace",
+	suit: "hearts",
+	cardImage: "images/ace_of_hearts.png"
+	},
+	{
+	rank: "jack",
+	suit: "diamonds",
+	cardImage: "images/jack_of_diamonds.png"
+	},
+	{
+	rank: "jack",
+	suit: "hearts",
+	cardImage: "images/jack_of_hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king_of_diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king_of_hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen_of_diamonds.png"
+	},
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen_of_hearts.png"
+	}
+]
